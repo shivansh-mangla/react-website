ec9a389618cd637570bdd47b9dad1b0342d62561
Shivansh Mangla <shivanshmangla72@gmail.com>
2024-07-22 15:32:31 +0530

    Made basic cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,13 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Food from "./Food";
+import Card from "./Card"
 
 function App() {
-
   return(
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
-      <Footer/>
-      <Footer/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
     </>
   );
 }
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="Profile Picture" />
+            <h2 className="card-title">Shivansh Mangla</h2>
+            <p className="card-text">BTech Student in Thapar University. Likes cricket</p>
+        </div>
+    );
+}
+
+export default Card
